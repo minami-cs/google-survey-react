912471f93f9a9069c3689e3b5b872e8ce361035c
Mina <mina9301@gmail.com>
2022-03-29 23:16:30 +0900

    Add: 글로벌 스타일 및 테마 생성

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+button {
+  background-color: transparent;
+  outline: none;
+  border: 0;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+}
+
+a {
+  text-decoration: none;
+}
+`;
+export default GlobalStyle;
---
+++ src/styles/theme.tsx
@@ -0,0 +1,13 @@
+const theme = {
+  basicPurple: '#673ab7',
+  lightPurple: '#E1D8F1',
+  backgroundColor: '#f0ebf8',
+  titleColor: '#202124',
+  borderColor: '#dadce0',
+  lightGrey: '#b9b9b9',
+  buttonGrey: '#bdbdbd',
+  iconGrey: '#5f6368',
+  selectedBlue: '#4285f4',
+};
+
+export default theme;
