5a43481d52f6f670b59caf011dacb5dac13b35f7
Mina <mina9301@gmail.com>
2022-04-01 01:25:48 +0900

    Modify: Question의 드롭다운 메뉴 내용 및 컴포넌트 UI 스타일 수정

--- src/components/FormEdit/Question.tsx
+++ src/components/FormEdit/Question.tsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 import { MenuItem, Select, TextField } from '@mui/material';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import SubjectIcon from '@mui/icons-material/Subject';
+import ShortTextIcon from '@mui/icons-material/ShortText';
+import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 
 export default function Question() {
   return (
@@ -11,12 +16,27 @@ export default function Question() {
           defaultValue="제목없는 질문"
           placeholder="질문"
         />
-        <SelectType>
-          <TypeItem value="short">단답형</TypeItem>
-          <TypeItem value="long">장문형</TypeItem>
-          <TypeItem value="radio">객관식 질문</TypeItem>
-          <TypeItem value="checkbox">체크박스</TypeItem>
-          <TypeItem value="dropdown">드롭다운</TypeItem>
+        <SelectType defaultValue="radio">
+          <TypeItem value="short">
+            <ShortTextIcon />
+            <ItemText>단답형</ItemText>
+          </TypeItem>
+          <TypeItem value="long">
+            <SubjectIcon />
+            <ItemText>장문형</ItemText>
+          </TypeItem>
+          <TypeItem value="radio">
+            <RadioButtonCheckedIcon />
+            <ItemText>객관식 질문</ItemText>
+          </TypeItem>
+          <TypeItem value="checkbox">
+            <CheckBoxIcon />
+            <ItemText>체크박스</ItemText>
+          </TypeItem>
+          <TypeItem value="dropdown">
+            <ArrowDropDownCircleIcon />
+            <ItemText>드롭다운</ItemText>
+          </TypeItem>
         </SelectType>
       </Header>
       <Content>
@@ -64,10 +84,20 @@ const Title = styled(TextField)(
 const SelectType = styled(Select)(
   ({ theme }) => `
   margin-left: 20px;
-
+  
   .MuiSelect-select {
+    display: flex;
+    vertical-align: middle;
     width: 208px;
+    font-size: 0.875rem;
+    color: ${theme.iconGrey};
+    
+    .MuiSvgIcon-root	{
+      color: ${theme.iconGrey};
+      margin-right: 16px;
+    }
   }
+  
 
   .MuiInputBase-root {
     &:focus {
@@ -77,7 +107,25 @@ const SelectType = styled(Select)(
 `
 );
 
-const TypeItem = styled(MenuItem)(({ theme }) => ``);
+const TypeItem = styled(MenuItem)(
+  ({ theme }) => `
+  .MuiMenuItem-root {
+    color: ${theme.iconGrey};
+  }
+
+  .MuiSvgIcon-root	{
+    margin-right: 16px;
+    color: ${theme.iconGrey};
+  }
+`
+);
+
+const ItemText = styled.p(
+  ({ theme }) => `
+  font-size: 0.875rem;
+  color: ${theme.iconGrey};
+`
+);
 
 const Content = styled.div(({ theme }) => ``);
 
